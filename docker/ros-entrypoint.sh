40f8c4d2413636d96b561e1af92a3aeb03785500
Thomas Wright <tom.tdw@gmail.com>
2024-06-14 00:10:19 +0200

    Working integration of different services including rosmqttbridge

--- docker/ros-entrypoint.sh
+++ docker/ros-entrypoint.sh
@@ -4,8 +4,6 @@
 # Source ROS 2
 source /opt/ros/humble/setup.bash
 source /opt/scan_relay_ws/install/setup.bash
-# Set the RMW implementation to Cyclone DDS to fix issues with scan
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 
 # Source the base workspace, if built
 if [ -f /opt/setup.bash ]
